7a43b868421e8435381f5f9d522695d0ee9580ec
Kolesnikov Roman <kolesnikov.roman.vl@gmail.com>
2020-09-29 23:53:38 +0300

    add image_picker, path, path_provider, update taking picture for image input

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,6 +11,24 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+  final ImagePicker _imagePicker = ImagePicker();
+
+  Future<void> _takePicture() async {
+    final PickedFile image = await _imagePicker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if(image != null) {
+      final File imageFile = File(image.path);
+      setState(() {
+        this._storedImage = imageFile;
+      });
+      final Directory appDir = await syspaths
+          .getApplicationDocumentsDirectory();
+      final String fileName = path.basename(imageFile.path);
+      final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +57,7 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: FlatButton.icon(
-            onPressed: () {},
+            onPressed: this._takePicture,
             icon: Icon(Icons.camera),
             label: Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
