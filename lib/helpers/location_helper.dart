0b579c2578aab194483e41d0cb2c87fedac6d9cc
Kolesnikov Roman <kolesnikov.roman.vl@gmail.com>
2020-10-10 19:52:09 +0300

    update map style

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -6,7 +6,7 @@ import '../constants.dart';
 class LocationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
-    return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+f00(${longitude},${latitude})/${longitude},${latitude},16,0/600x300?access_token=${MAP_API_KEY}';
+    return 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/pin-s-a+f00(${longitude},${latitude})/${longitude},${latitude},16,0/600x300?access_token=${MAP_API_KEY}';
   }
 
   static Future<String> getPlaceAddress(double lat, double lng) async {
